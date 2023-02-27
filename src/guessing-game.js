cd22513af191c3a60a45927b6d775c54de4ea538
spvvrv <s.pivovarova@rambler.ru>
2023-02-27 13:06:39 +0300

    add class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,44 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.guessNumber = Math.round((this.min + this.max) / 2);
+    return this.guessNumber;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.guessNumber;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.guessNumber;
+  }
 }
 
 module.exports = GuessingGame;
+
+// let arr = [-1, 0, 3, 5, 7, 9, 12];
+
+// function binarySearch(arr, key) {
+//   let start = 0;
+//   let end = arr.length - 1;
+
+//   while (start <= end) {
+//     let middle = Math.floor((start + end) / 2);
+
+//     if (arr[middle] === key) {
+//       return middle;
+//     } else if (arr[middle] < key) {
+//       //если средний элемент меньше ключа, ищите в правой половине
+//       start = middle + 1;
+//     } else { //если средний элемент болльше ключа, ищите в левой половине
+//       end = middle - 1;
+//     }
+//   }
+//   return -1;
+// }
